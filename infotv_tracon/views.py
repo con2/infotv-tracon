eee725353bee314a999c6c8d9d06959ac9f8b476
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-03 21:56:30 +0300

    fix edit redirect

--- infotv_tracon/views.py
+++ infotv_tracon/views.py
@@ -1,8 +1,9 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.core.urlresolvers import reverse
+from django.conf import settings
 from django.shortcuts import redirect
 
 
-@staff_member_required
+@staff_member_required(login_url=settings.LOGIN_URL)
 def infotv_edit_redirect_view(self, event):
     return redirect(reverse('infotv_view', args=(event,)) + '?edit=1')
