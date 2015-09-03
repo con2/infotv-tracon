40966b090a916387d61cb77054e8f4d0426a5d04
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-03 22:01:53 +0300

    fix login_required

--- infotv_tracon/views.py
+++ infotv_tracon/views.py
@@ -1,9 +1,9 @@
-from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.admin.views.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.shortcuts import redirect
 
 
-@staff_member_required(login_url=settings.LOGIN_URL)
+@login_required
 def infotv_edit_redirect_view(self, event):
     return redirect(reverse('infotv_view', args=(event,)) + '?edit=1')
