8202661b01eba8d094c3ff479180308ea06fa99a
Santtu Pajukanta <santtu@pajukanta.fi>
2015-09-04 09:21:33 +0300

    redirect to ?slow=1 by default

--- infotv_tracon/urls.py
+++ infotv_tracon/urls.py
@@ -13,7 +13,7 @@ from .views import infotv_edit_redirect_view
 
 urlpatterns = patterns('',
     url(r'', include('kompassi_oauth2.urls')),
-    url(r'^$', RedirectView.as_view(url="/events/{event_slug}/infotv/".format(event_slug=settings.INFOTV_DEFAULT_EVENT_SLUG))),
+    url(r'^$', RedirectView.as_view(url="/events/{event_slug}/infotv/?slow=1".format(event_slug=settings.INFOTV_DEFAULT_EVENT_SLUG))),
     url(r'^events/(?P<event>[a-z0-9-]+)/infotv/?$', csrf_exempt(InfoTvView.as_view()), name='infotv_view'),
     url(r'^events/(?P<event>[a-z0-9-]+)/infotv/edit/?$', infotv_edit_redirect_view, name='infotv_edit_redirect_view'),
     url(r'^admin/', include(admin.site.urls)),
