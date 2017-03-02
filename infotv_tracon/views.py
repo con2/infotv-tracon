b387a25a10b1575387f89fb1b84b2ab15c4daf3d
Santtu Pajukanta <santtu@pajukanta.fi>
2017-03-02 19:21:18 +0200

    try to polyfill Object.assign

--- infotv_tracon/views.py
+++ infotv_tracon/views.py
@@ -19,6 +19,33 @@ infotv.views.TEMPLATE = u"""
       webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
     }
     </script>
+    <script>
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+    if (typeof Object.assign != 'function') {
+    Object.assign = function(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+            }
+            }
+        }
+        }
+        return to;
+    };
+    }
+    </script>
 </head>
 <body>
     <div id="tv"></div>
